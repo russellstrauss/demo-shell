19bcb0a79e4a9010efca4999582b1567131adac2
RIGHTPOINT\RStrauss <russell_oasis@bitbucket.org>
2017-08-18 10:47:41 -0400

    Adding bootstrap glyphicons and moving responsive scaffolding into mixin format

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
-var path = require('path');
+// var path = require('path');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
 	return gulp.src('./assets/sass/main.scss')
 	.pipe(sass().on('error', sass.logError))
-		.pipe(sass({
-			paths: [ path.join(__dirname, 'sass', 'includes') ]
-		}))
+		.pipe(sourcemaps.init())
+			//.pipe(sass({outputStyle: 'compressed'})) // minified
+			.pipe(sass())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('./assets/sass'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-		gulp.watch('./assets/sass/**/*.scss', ['sass']);
+	gulp.watch('./assets/sass/**/*.scss', ['sass']);
 });
 
 // Default Task
