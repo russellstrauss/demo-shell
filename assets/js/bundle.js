01a4cb45a581cdbd7927284a03c2d96ee866cd94
RIGHTPOINT\RStrauss <russell_oasis@bitbucket.org>
2017-10-17 17:25:01 -0400

    Adding BrowserSync for rapid development, updating CSS architecture to inverted triangle CSS

---
+++ assets/js/bundle.js
@@ -0,0 +1,114 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = function() {
+	
+	var settings;
+	
+	return {
+		
+		settings: {
+			
+		},
+		
+		init: function() {
+			
+			settings = this.settings;
+			
+			this.bindUI();
+		},
+		
+		bindUI: function() {
+			
+			
+						
+		}
+	}
+}
+},{}],2:[function(require,module,exports){
+var Component = require('./components/component.js');
+var Utilities = require('./utils.js');
+
+(function () {
+	
+	$(document).ready(function() {
+				
+		Component().init();
+		
+		$(window).trigger('resize');
+	
+	});
+	
+})();
+},{"./components/component.js":1,"./utils.js":3}],3:[function(require,module,exports){
+(function () {
+	
+	var appSettings;
+	
+	window.utils = (function() {
+		
+		return {
+			appSettings: {
+				mobileMax: 767,
+				tabletMin: 768,
+				tabletMax: 991,
+				desktopMin: 992,
+				desktopLargeMin: 1200
+			},
+			
+			mobile: function() {
+				return window.innerWidth < appSettings.tabletMin;
+			},
+			
+			tablet: function() {
+				return (window.innerWidth > appSettings.mobileMax && window.innerWidth < appSettings.desktopMin);
+			},
+			
+			debounce: function(func, wait, immediate) {
+				var timeout;
+				return function () {
+					var context = this, args = arguments;
+					var later = function () {
+						timeout = null;
+						if (!immediate) func.apply(context, args);
+					};
+					var callNow = immediate && !timeout;
+					clearTimeout(timeout);
+					timeout = setTimeout(later, wait);
+					if (callNow) func.apply(context, args);
+				};
+			},
+			
+			isOnScreen: function(element) {
+
+				var win = $(window);
+				
+				var viewport = {
+					top: win.scrollTop(),
+					left: win.scrollLeft()
+				};
+				viewport.right = viewport.left + win.width();
+				viewport.bottom = viewport.top + win.height();
+
+				var bounds = element.offset();
+				bounds.right = bounds.left + element.outerWidth();
+				bounds.bottom = bounds.top + element.outerHeight();
+
+				return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+			},
+			
+			getBreakpoint: function() {
+				if (window.innerWidth < appSettings.tabletMin) return 'mobile';
+				else if (window.innerWidth < appSettings.desktopMin) return 'tablet';
+				else return 'desktop';
+			},
+			
+			secondsToMilliseconds: function(seconds) {
+				return seconds * 1000;
+			}
+		}
+	})();
+
+	module.exports = window.utils;
+
+})();
+},{}]},{},[2]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,71 @@
-// Include gulp
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
-// var path = require('path');
 var sourcemaps = require('gulp-sourcemaps');
+var browserify = require('browserify');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream'); // required to dest() for browserify
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var gutil = require('gulp-util');
 
 gulp.task('sass', function () {
 	return gulp.src('./assets/sass/main.scss')
-	.pipe(sass().on('error', sass.logError))
-		.pipe(sourcemaps.init())
-			//.pipe(sass({outputStyle: 'compressed'})) // minified
-			.pipe(sass())
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./assets/sass'));
+	.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError)) // .on('error', sass.logError) prevents gulp from crashing when saving a typo or syntax error
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('./assets/sass'))
+	.pipe(browserSync.stream()); // causes injection of styles on save
 });
 
-// Watch Files For Changes
+gulp.task('sync', ['sass'], function() {
+	browserSync.init({
+		open: true,
+		server: {
+			baseDir: "./",
+		}
+	});
+});
+
+var vendors = {
+	merge: [
+		'./assets/vendors/js/jquery-2.2.3.min.js', 
+		'./assets/vendors/js/bootstrap.min.js'
+	]
+};
+
+gulp.task('vendors', function() {
+	return gulp.src(vendors.merge)
+		.pipe(concat('vendors.js'))
+		.pipe(gulp.dest('./assets/vendors/js/'));
+});
+
+gulp.task('javascript', function() {
+
+	var bundleStream = browserify('./assets/js/main.js').bundle();
+
+	return bundleStream
+		.pipe(source('main.js'))
+		.pipe(rename('bundle.js'))
+		.pipe(gulp.dest('./assets/js/'))
+		.pipe(browserSync.stream());
+		
+		bundleStream.on('error', function(e) {
+			gutil.log(e);
+		});
+});
+
+gulp.task('HTML', function() {
+	return gulp.src(['./index.html'])
+		.pipe(browserSync.stream()); // causes injection of html changes on save
+});
+
 gulp.task('watch', function() {
 	gulp.watch('./assets/sass/**/*.scss', ['sass']);
+	gulp.watch('./assets/js/**/*.js', ['javascript']);
+	gulp.watch('./**/*.html', ['HTML']);
+	gulp.watch(['./assets/vendors/js/*.js', '!./assets/vendors/js/vendors.min.js'], ['vendors']);
 });
 
 // Default Task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['vendors', 'javascript', 'sass', 'watch', 'sync']);
